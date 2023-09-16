89137d546f3c907e512f5154c7def8824eb4359c
GoodShotMates <142911229+GoodShotMates@users.noreply.github.com>
2023-09-16 11:23:00 +0700

    Delete javascript directory

--- javascript/java/dice.js
+++
@@ -1,7 +0,0 @@
-let number;
-
-document.getElementById("roll").onclick = function () {
-    number = Math.floor(Math.random()* 6);
-    
-    document.getElementById("number").innerHTML = number;
-}
